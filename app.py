6bf93989e981477bf6343cbdb16ab4073071e55c
chinu2004 <prakashchinu55@gmail.com>
2025-08-24 10:56:33 +0530

    done

--- app.py
+++ app.py
@@ -178,5 +178,5 @@ def mark_attendance():
 
 # ------------------ Run ------------------
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000 , debug=True)
+    app.run(host='0.0.0.0', port=5000)
 
